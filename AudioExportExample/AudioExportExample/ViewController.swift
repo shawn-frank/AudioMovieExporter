f2528fa9b8373ebcfff0bcd869feae58167b3a24
shawn-frank <shawn@shawn.design>
2022-02-20 20:25:08 +0400

    Initial XCode project

---
+++ AudioExportExample/AudioExportExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AudioExportExample
+//
+//  Created by Shawn Frank on 20/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
